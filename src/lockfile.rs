76f0d4c9b769f3157d0dd6288a403d721a313d2e
Edward Yang <edward.yang6771@gmail.com>
2016-08-09 04:05:43 -0500

    Implement lockfile

--- src/lockfile.rs
+++ src/lockfile.rs
@@ -1,2 +1,33 @@
 use std::env::temp_dir;
+use std::path::PathBuf;
+use std::fs::{File, remove_file};
 
+lazy_static! {
+    static ref TMPDIR: PathBuf = temp_dir().to_path_buf();
+}
+
+pub struct LockFile {
+    pb: PathBuf
+}
+
+impl LockFile {
+    pub fn new(name: &str) -> Option<Self> {
+        let path = TMPDIR.join(name);
+        if path.exists() {
+            None
+        } else {
+            match File::create(&path) {
+                Ok(_) => Some(LockFile {
+                    pb: path.to_path_buf()
+                }),
+                Err(_) => None
+            }
+        }
+    }
+}
+
+impl Drop for LockFile {
+    fn drop(&mut self) {
+        remove_file(&self.pb).unwrap();
+    }
+}
